638961aae486ce0cc5fc0226bdda6319476bac99
Madis Janson <mzz@linux.ee>
2024-03-29 02:17:41 +0200

    Use WiFiUdp

--- lightclock.ino
+++ lightclock.ino
@@ -1,11 +1,9 @@
 #include <esp_task_wdt.h>
 #include <esp_bt.h>
 #include "WiFi.h"
-#include "AsyncUDP.h"
-#include <lwip/netdb.h>
+#include "WiFiUdp.h"
 #include <sys/time.h>
 #include "wifi-conf.h"
-#include <atomic>
 
 #define CLOCK_ID 0
 
@@ -51,97 +49,88 @@ struct TimeMessage {
 static const char host_name[] =
   { 'l', 'i', 'g', 'h', 't', 'c', 'l', 'o', 'c', 'k', '0' + CLOCK_ID, '\0' };
 
-static AsyncUDP udp;
+static WiFiUDP udp;
+static IPAddress server_addr;
+static int net_state = NS_NONE;
 RTC_NOINIT_ATTR uint32_t server_ip;
-// These fields should be accessed via getConfig/setConfig
 RTC_NOINIT_ATTR TimeMessage config;
-RTC_NOINIT_ATTR time_t last_conf_update;
-static SemaphoreHandle_t configMutex = 0;
-static std::atomic_int net_state(NS_NONE);
-
-#define LOCK_CONF(a) while (!xSemaphoreTake(configMutex, portMAX_DELAY)) delay(1);\
-  a; xSemaphoreGive(configMutex)
-
-static TimeMessage getConfig(time_t *last_update) {
-  time_t tmp;
-  if (!last_update) {
-    last_update = &tmp;
-  }
-  LOCK_CONF(TimeMessage result(config); *last_update = last_conf_update);
-  return result;
-}
-
-static void setConfig(const TimeMessage& msg, time_t cur_t) {
-  LOCK_CONF(config = msg; last_conf_update = cur_t);
-}
-
-static bool resolve_server_ip() {
-  if (!server_ip) {
-    struct hostent *addr = gethostbyname(CONF_SERVER);
-    if (!addr) {
-      PRINT("Unknown host\n");
-      return false;
-    }
-    memcpy(&server_ip, addr->h_addr_list[0], 4);
-    PRINTF("SERVER %x\n", server_ip);
-  }
-  return true;
-}
+RTC_NOINIT_ATTR time_t last_update;
 
 static bool establish_udp_conn() {
   WiFi.setSleep(false);
-  if (!resolve_server_ip() || !udp.connect(IPAddress(server_ip), CONF_SERVER_PORT)) {
-    PRINT("Couldn't connect\n");
-    return false;
-  }
-  PRINT("UDP connected. ");
-  udp.onPacket([](AsyncUDPPacket packet) {
-    WiFi.setSleep(true);
-    net_state = NS_CONNECTED;
-    TimeMessage response;
-    if (packet.length() == sizeof response) {
-      memcpy(&response, packet.data(), sizeof response);
-      if (response.request_id == getConfig(0).request_id) {
-        time_t cur_t;
-        time(&cur_t);
-        PRINTF("CONF s_on=%d f_on=%d s_off=%d f_off=%d\n",
-               (int) response.start_on, (int) response.finish_on,
-               (int) response.start_off, (int) response.finish_off);
-        if (cur_t < response.clock_id - 1 ||
-            cur_t > response.clock_id + 1) {
-          timeval epoch = { response.clock_id, 0 };
-          settimeofday(&epoch, 0);
-          time(&cur_t);
-          PRINTF("Updated time: %d (%d); ", (int) cur_t, (int) response.clock_id);
-          PRINT(ctime(&cur_t));
-        } else {
-          PRINTF("Time OK %d (%d); ", (int) cur_t, (int) response.clock_id);
-          PRINT(ctime(&cur_t));
-        }
-        setConfig(response, cur_t);
-      } else {
-        PRINT("Invalid request_id in response\n");
-      }
-    } else {
-      PRINT("Invalid response length\n");
-    }
-  });
-  return true;
+  return udp.begin((esp_random() & 0x1fff) + 1);
 }
 
-static void request_light_data() {
+static bool request_light_data() {
+  if (!server_ip) {
+    if (!udp.beginPacket(CONF_SERVER, CONF_SERVER_PORT)) {
+      PRINT("Could not resolve " CONF_SERVER "\n");
+      return false;
+    }
+    server_addr = udp.remoteIP();
+    server_ip = (uint32_t) server_addr;
+    PRINTF("SERVER %x\n", server_ip);
+  } else {
+    server_addr = IPAddress(server_ip);
+    if (!udp.beginPacket(server_addr, CONF_SERVER_PORT)) {
+      return false;
+    }
+  }
   struct timeval tv;
   gettimeofday(&tv, 0);
   int64_t random = esp_random() ^ ((uint64_t) tv.tv_usec << 32);
-  LOCK_CONF(config.request_id = random ^ (config.request_id << 16);
-    TimeMessage request = config);
+  config.request_id = random ^ (config.request_id << 16);
+  TimeMessage request = config;
   request.clock_id = CLOCK_ID; // this clock
   udp.write((const uint8_t*) &request, sizeof request);
-  PRINTF("Sent request %lu\n", sizeof request);
+  PRINT("Sent request\n");
+  return udp.endPacket();
+}
+
+static bool receive_light_data() {
+  if (udp.parsePacket() <= 0) {
+    return false;
+  }
+  if (udp.remoteIP() != server_addr || udp.remotePort() != CONF_SERVER_PORT) {
+    PRINTF("Invalid packet source %x:%u\n", (uint32_t) udp.remoteIP(), (unsigned) udp.remotePort());
+    udp.flush();
+    return false;
+  }
+  TimeMessage response;
+  char buf[sizeof response + 1];
+  int count = udp.read(buf, sizeof buf);
+  udp.flush();
+  if (count != sizeof response) {
+    PRINT("Invalid response length\n");
+    return false;
+  }
+  memcpy(&response, buf, sizeof response);
+  if (response.request_id != config.request_id) {
+    PRINT("Invalid request_id in response\n");
+    return false;
+  }
+  time_t cur_t;
+  time(&cur_t);
+  PRINTF("CONF s_on=%d f_on=%d s_off=%d f_off=%d\n",
+         (int) response.start_on, (int) response.finish_on,
+         (int) response.start_off, (int) response.finish_off);
+  if (cur_t < response.clock_id - 1 ||
+      cur_t > response.clock_id + 1) {
+    timeval epoch = { response.clock_id, 0 };
+    settimeofday(&epoch, 0);
+    time(&cur_t);
+    PRINTF("Updated time: %d (%d); ", (int) cur_t, (int) response.clock_id);
+    PRINT(ctime(&cur_t));
+  } else {
+    PRINTF("Time OK %d (%d); ", (int) cur_t, (int) response.clock_id);
+    PRINT(ctime(&cur_t));
+  }
+  config = response;
+  last_update = cur_t;
+  return true;
 }
 
 void setup() {
-  configMutex = xSemaphoreCreateMutex();
 #if SERIAL
   Serial.begin(115200);
 #endif
@@ -165,7 +154,7 @@ void setup() {
     PRINT("Setup from boot/reset\n");
     // Zero RTC variables
     server_ip = 0;
-    last_conf_update = 0;
+    last_update = 0;
     memset(&config, 0, sizeof config);
     int i;
     for (i = 0; i < PWM_MAX / 16; ++i) {
@@ -183,18 +172,17 @@ void setup() {
 }
 
 static int update_light(int *light_on) {
-  TimeMessage cfg = getConfig(0);
-  if (!cfg.ok()) {
-    PRINTF("No config for light ns=%d (%d >= %d)\n", net_state.load(), (int) cfg.finish_on, (int) cfg.start_off);
+  if (!config.ok()) {
+    PRINTF("No config for light ns=%d (%d >= %d)\n", net_state, (int) config.finish_on, (int) config.start_off);
     return 200;
   }
-  PRINTF("ns=%d ", net_state.load());
+  PRINTF("ns=%d ", net_state);
 
   struct timeval tv;
   gettimeofday(&tv, 0);
   int64_t t = tv.tv_sec;
-  if (t > cfg.finish_off) {
-    t -= ((t - cfg.finish_off) / DAY + 1) * DAY;
+  if (t > config.finish_off) {
+    t -= ((t - config.finish_off) / DAY + 1) * DAY;
   }
 
   int loop_time = UPDATE_POLL_MS;
@@ -204,27 +192,27 @@ static int update_light(int *light_on) {
       delay(timestep);
     }
     int brightness = 0;
-    if (t < cfg.start_on) {
-      timestep = (cfg.start_on - t) * 1000 - (tv.tv_usec / 1000);
+    if (t < config.start_on) {
+      timestep = (config.start_on - t) * 1000 - (tv.tv_usec / 1000);
       PRINTF("%lgs until start ", (double) timestep / 1000.0);
-    } else if (t >= cfg.finish_off) {
+    } else if (t >= config.finish_off) {
       timestep = MAX_WAIT_MS;
       PRINT("light has already gone off ");
     } else {
       int64_t dt, total;
-      if (t <= cfg.finish_on) {
+      if (t <= config.finish_on) {
         //PRINT("FADE-IN ");
-        dt = ((t - cfg.start_on) * 1000) + (tv.tv_usec / 1000);
-        total = (cfg.finish_on - cfg.start_on) * 1000;
+        dt = ((t - config.start_on) * 1000) + (tv.tv_usec / 1000);
+        total = (config.finish_on - config.start_on) * 1000;
         timestep = total / PWM_MAX;
-      } else if (t >= cfg.start_off) {
+      } else if (t >= config.start_off) {
         //PRINT("FADE-OFF ");
-        dt = ((cfg.finish_off - t) * 1000) - (tv.tv_usec / 1000);
-        total = (cfg.finish_off - cfg.start_off) * 1000;
+        dt = ((config.finish_off - t) * 1000) - (tv.tv_usec / 1000);
+        total = (config.finish_off - config.start_off) * 1000;
         timestep = total / PWM_MAX;
       } else {
         dt = total = 1;
-        timestep = (cfg.start_off - t) * 1000 - (tv.tv_usec / 1000);
+        timestep = (config.start_off - t) * 1000 - (tv.tv_usec / 1000);
         PRINTF("FULL-ON %lgs until fade-off ", (double) timestep / 1000.0);
       }
       brightness = dt * PWM_MAX / total;
@@ -269,8 +257,7 @@ void loop() {
   esp_task_wdt_reset();
 
   time_t t = time(NULL);
-  time_t last_update;
-  if (!getConfig(&last_update).ok() || !last_update || t > last_update + 25) {
+  if (!config.ok() || !last_update || t > last_update + 25) {
     if (timeout_counter >= 20000 / UPDATE_POLL_MS) {
       WiFi.disconnect(true);
       net_state = NS_NONE;
@@ -279,7 +266,13 @@ void loop() {
     switch (net_state) {
       case NS_UPDATING:
         if (WiFi.isConnected()) {
-          ++timeout_counter;
+          if (receive_light_data()) {
+            net_state = NS_CONNECTED;
+	    timeout_counter = 0;
+            WiFi.setSleep(true);
+          } else {
+            ++timeout_counter;
+          }
           break;
         }
       case NS_NONE:
@@ -298,6 +291,10 @@ void loop() {
         net_state = NS_UPDATING;
         timeout_counter = 0;
         request_light_data();
+    }
+  } else if (WiFi.isConnected()) {
+    while (udp.parsePacket() > 0) {
+      udp.flush(); // discard unexpected packet
     }
   }
   digitalWrite(LED_BUILTIN, WiFi.isConnected() ? HIGH : LOW);
@@ -319,10 +316,10 @@ void loop() {
     esp_sleep_enable_timer_wakeup(wait * 1000);
     PRINTF("Sleep for %dms, light=%d\n", wait, light_on);
     FLUSH();
-    if (wait < 25000 || light_on) {
+    //if (wait < 25000 || light_on) {
       esp_light_sleep_start();
-    } else {
-      esp_deep_sleep_start();
-    }
+    //} else {
+    //  esp_deep_sleep_start();
+    //}
   }
 }
